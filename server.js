6fcfdeefe6f02ec19d6e5d370513b54b4fddf6bc
nibague <nikolasibagues@gmail.com>
2022-05-12 22:30:42 -0500

    definiendo operaciones de comunicacion patch delete para el CRUD

--- server.js
+++ server.js
@@ -3,19 +3,40 @@
 
 //nuevo import 
 import Express from "express";
+import { MongoClient, ObjectId} from 'mongodb';
+import Cors from 'cors';
+
+const stringConexion = 'mongodb+srv://Nibague:uribeparaco82@clustertest.tebmq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+
+const client = new MongoClient(stringConexion,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+let conexion;
 
 const app = Express();
+app.use(Cors());
+
 app.use(Express.json());
 
 app.get('/vehiculos', (req, res)=>{
     console.log('sending request get in the route /vehiculos');
-    const vehiculos = [
+    //codigo que permite traer los vehiculos de la base de datos
+    //para evitar probar con datos quemados:
+    /*const vehiculos = [
         { nombre: 'corolla', marca: 'ferrari', modelo: '2011'},
         { nombre: 'fiesta', marca: 'chevrolet', modelo: '2017'},
         { nombre: 'tesla', marca: 'tesla', modelo: '2009'},
         { nombre: 'yaris', marca: 'ford', modelo: '2004'}
-    ];
-    res.send(vehiculos); 
+    ];*/
+    conexion.collection('vehiculo').find({}).limit(50).toArray((err, result)=>{
+        if(err){
+            res.sendStatus(500).send('error al consultar vehiculos');
+        }else{
+            res.json(result);
+        }
+    }); 
 });
 
 // solicitudes de tipo post no se pueden probar en el navegador
@@ -31,7 +52,16 @@ app.post('/vehiculos/nuevo', (req, res)=>{
         && Object.keys(datosVehiculo).includes('model'))
         {
              //implementar codigo para crear vehiculo en la base de datos
-             res.sendStatus(200);
+             conexion.collection('vehiculo').insertOne(datosVehiculo, (err, result)=>{
+                 if(err){
+                     console.error(err)
+                     res.sendStatus(500);
+                 }else{
+                     console.log(result)
+                     res.sendStatus(200);
+                 }
+
+             });
          }else{
              res.sendStatus(500);
          }
@@ -44,7 +74,52 @@ app.post('/vehiculos/nuevo', (req, res)=>{
 
 });
 
-app.listen(5000, ()=>{
-    console.log('listen port 5000')
+app.patch('/vehiculos/editar', (req, res)=>{
+    const edicion = req.body;
+    console.log(edicion);
+    const filtroVehiculo = {_id: new ObjectId(edicion.id)}
+    delete edicion.id
+    const operacion = {
+        $set:edicion,
+    };
+    conexion.collection('vehiculo').findOneAndUpdate(filtroVehiculo, operacion, {upsert:true, returnOriginal: true}, (err, result)=>{
+        if(err){
+            console.error('error actualizando vehiculo', err);
+            res.sendStatus(500);
+        }else{
+            console.log('actualizado con exito');
+            res.sendStatus(200);
+        }
+    });
+
 });
 
+app.delete('/vehiculos/eliminar', (req, res)=>{
+    const edicion = req.body
+    const filtroVehiculo = {_id: new ObjectId(edicion.id)} 
+    conexion.collection('vehiculo').deleteOne(filtroVehiculo, (err, result) =>{
+        if(err){
+            console.error(err);
+            res.sendStatus(500)
+        }else{
+            res.sendStatus(200);
+        }
+    })
+})
+
+const main = ()=> {
+    client.connect((err, db)=>{
+        if(err){
+            console.erro('Error conectando a la base de datos');
+            return 'error';
+        }
+        conexion = db.db('concesionario');
+        console.log('succesful conection!')
+        return app.listen(5000, ()=>{
+            console.log('listen port 5000');
+        });
+    });
+    
+};
+
+main();
